3ebec98f972de210690d84352bc140926f5149d9
StefanSteuge <fotosteuge@gmail.com>
2023-12-21 17:32:03 +0200

    added constant for managing

--- src/main/java/org/example/util/Constants.java
+++ src/main/java/org/example/util/Constants.java
@@ -3,4 +3,17 @@ package org.example.util;
 public class Constants {
     public static final String EQUATION_REGEX = "^[-+]?[0-9]*\\.?[0-9]*[xX][^0-9]*=[^0-9]*[-+]?[0-9]*\\.?[0-9]*$";
     public static final String EQUATION_REGEX_2 = ".*[+\\-*/]{2,}.*";
+    public static final String FINALE_MESSAGE = "Goodbye!";
+    public static final String MESSAGE_TO_THE_READERS = """
+            HELLO TO THE MATH HELPER!!!
+            
+            PLEASE, SELECT ONE OF THE FOLLOWING ACTIONS BY TYPING
+            THE OPTION'S NUMBER AND PRESSING ENTER KEY:
+            
+            [1] SAVE EQUATION
+            [2] SAVE ROOT
+            [3] FIND EQUATION BY ROOT
+            [4] EVALUATE EQUATION
+            
+            TYPE "EXIT" TO STOP THE PROGRAM AND EXIT!""";
 }
